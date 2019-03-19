e98482139a8ac166c36fe73d1a923e814386f2de
kmj8843 <47032060+kmj8843@users.noreply.github.com>
2019-03-19 21:38:00 +0900

    CameraScripts
    CameraScripts

--- HASTEN/Assets/04. Scripts/CameraCtrl.cs
+++ HASTEN/Assets/04. Scripts/CameraCtrl.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraCtrl : MonoBehaviour
 {
+    private Camera myCamera;
+    public GameObject target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myCamera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 targetPos = target.transform.position;
+        myCamera.transform.position = new Vector3(targetPos.x, targetPos.y + 6, targetPos.z - 8);
     }
 }
