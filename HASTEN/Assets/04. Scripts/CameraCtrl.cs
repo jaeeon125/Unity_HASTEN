1d5e3f093c41f0c83be6ba01696e09f99b9ed14e
LeaJaeHeon <39754095+LeeJaeHeon1@users.noreply.github.com>
2019-03-19 21:11:53 +0900

    CreateGameManager
    CreateGameManager

--- HASTEN/Assets/04. Scripts/CameraCtrl.cs
+++ HASTEN/Assets/04. Scripts/CameraCtrl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class CameraCtrl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
