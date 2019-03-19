335f50417ea56945a21b00d9352756944aabfa36
LeaJaeHeon <39754095+LeeJaeHeon1@users.noreply.github.com>
2019-03-19 21:58:42 +0900

    IUNIT
    플레이어 컨트롤 삭제, 유닛 인터페이스

--- HASTEN/Assets/04. Scripts/Unit/IUnit.cs
+++ HASTEN/Assets/04. Scripts/Unit/IUnit.cs
@@ -5,4 +5,25 @@ using UnityEngine;
 public interface IUnit
 {
     int HP { get; set; }
+    int MP { get; set; }
+    int ARMOR { get; set; }
+    int SPEED { get; set; }
+    int POWER { get; set; }
+    bool ALIVE { get; set; }
+
+    void getDamage(int damage);
+}
+public class CUnit : IUnit
+{
+    public int HP { get { return HP; } set { HP = value; } }
+    public int MP { get { return MP; } set { MP = value; } }
+    public int ARMOR { get { return ARMOR; } set { ARMOR = value; } }
+    public int SPEED { get { return SPEED; } set { SPEED = value; } }
+    public int POWER { get { return POWER; } set { POWER = value; } }
+    public bool ALIVE { get { return ALIVE; } set { ALIVE = value; } }
+
+    public void getDamage(int damage)
+    {
+
+    }
 }
