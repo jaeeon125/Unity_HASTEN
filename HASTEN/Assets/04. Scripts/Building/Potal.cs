df8a9474d259f7df29f814b3415154fc6411d41d
LeaJaeHeon <39754095+LeeJaeHeon1@users.noreply.github.com>
2019-03-24 19:07:43 +0900

    Potal

---
+++ HASTEN/Assets/04. Scripts/Building/Potal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Potal : CommonBuilding
+{
+    public Transform LinkPotal;
+
+    public override void Active()
+    {
+        GameMgr.getInst().P_Trans.GetComponent<NavMeshAgent>().enabled = false;
+        GameMgr.getInst().P_Trans.position = LinkPotal.position;
+        GameMgr.getInst().P_Trans.GetComponent<NavMeshAgent>().enabled = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            GameMgr.getInst().P_Trans.GetComponentInChildren<PlayerCtrl>().potal = this;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            GameMgr.getInst().P_Trans.GetComponentInChildren<PlayerCtrl>().potal = null;
+    }
+}
