744adceb7428a93cce70b5d372cadc3de72d107d
kmj8843 <47032060+kmj8843@users.noreply.github.com>
2019-05-02 21:37:07 +0900

    5.2
    5.2

---
+++ HASTEN/Assets/04. Scripts/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private Inventory playerInven; //플레이어 인벤토리
+    public enum Type
+    {
+        Use, Mat
+    }
+    //string, 이미지, 설명, 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if(other.tag=="Item")
+        {
+            Debug.Log("A");
+            playerInven.gainItem(other.name);
+            Destroy(this);
+            
+        }
+    }
+}
