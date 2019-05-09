4dea9dd8fdc515a2159ddee0a703c3c9602e34ff
LeaJaeHeon <39754095+LeeJaeHeon1@users.noreply.github.com>
2019-05-09 21:43:23 +0900

    inventory
    Invetory

--- HASTEN/Assets/04. Scripts/Inventory.cs
+++ HASTEN/Assets/04. Scripts/Inventory.cs
@@ -2,6 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory 
+public class Inventory : MonoBehaviour
 {
+    private bool isOnInventory;
+    public GameObject inventory;
+
+    public void onInventory()
+    {
+        isOnInventory = !isOnInventory;
+        inventory.gameObject.SetActive(isOnInventory);
+    }
 }
